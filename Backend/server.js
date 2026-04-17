e3b6368df3d5d92f7753f7d98bf650917e0f2042
LegalPapersIndia <udit@documentsindia.com>
2026-04-17 17:31:30 +0530

    working

--- Backend/server.js
+++ Backend/server.js
@@ -25,9 +25,6 @@ app.use(cors({
   optionsSuccessStatus: 200
 }));
 
-// Handle preflight requests
-app.options('*', cors());
-
 // Middleware
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
